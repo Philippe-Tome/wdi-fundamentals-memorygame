0459beda1c430b467f4bf21d3eec10805d21d3b3
Philippe Tome <PTome@mar.local>
2019-10-17 21:35:30 +1100

    created game logic

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -1,8 +1,12 @@
 console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-
 console.log("User flipped " + cardOne);
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+alert("You found a match!");
